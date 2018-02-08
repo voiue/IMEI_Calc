1fe200a03823c8d6ecdc4f3aa292d037f0fa5073
anonymous <anonymous@foxmail.com>
2018-02-08 19:25:49 +0800

    Add a IMEI_Calc project

---
+++ IMEI_Calc/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace IMEI_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(263, 42);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 0;
+            this.buttonCalc.Text = "计算";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(87, 42);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(155, 21);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.Text = "12345678901234";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "14位IMEI：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "15位IMEI：";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(87, 90);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(155, 21);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(263, 90);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopy.TabIndex = 3;
+            this.buttonCopy.Text = "复制";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 169);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonCalc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMEI Calc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ IMEI_Calc/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMEI_Calc
+{
+    public partial class Form1 : Form
+    {
+        public int flag = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            //
+            if (textBoxInput.Text.Length != 14)
+            {
+                Regex reg = new Regex(@"[^0-9]"); // 排除型字符组(取反思想)
+                if (reg.IsMatch(textBoxInput.Text.ToString()))
+                {
+                    MessageBox.Show("仅能输入数字!");
+                    textBoxInput.Clear();
+                    goto Loop;
+                }
+
+                MessageBox.Show("请输入14位数字！");
+                textBoxInput.Clear();
+            }
+            else
+            {
+                //add alorgthom Method
+                //add result
+                textBoxOutput.Text = textBoxInput.Text + IMEI_Calc(textBoxInput.Text);
+            }
+        Loop:
+            { ;}
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            if (textBoxOutput.Text != "")
+            {
+                Clipboard.SetDataObject(textBoxOutput.Text);
+                //MessageBox.Show("已复制到剪贴板！");
+                timer1.Interval = 1000;
+                timer1.Enabled = true;
+            }
+        }
+        public int IMEI_Calc(string IMEI14)
+        {
+            //35890180697241
+            //358901806972417
+            int sum = 0;
+            int[] ME = new int[14];
+            //split string into a char array
+            for (int i = 0; i < 14; i++)
+            {
+                ME[i] = Convert.ToInt32(IMEI14.Substring(i, 1));
+            }
+            int[] ME_Even = new int[7];//even means ou shu
+            for (int i = 0; i < 7; i++)
+            {
+                ME_Even[i] = ME[i * 2 + 1] * 2;
+            }
+            int[] ME_Odd = new int[7];//odd means ji shu
+            for (int i = 0; i < 7; i++)
+            {
+                ME_Odd[i] = ME[i * 2];
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                sum += ME_Odd[i] + ME_Even[i] / 10 + ME_Even[i] % 10;
+            }
+            if (sum % 10 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return sum = 10 - sum % 10;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBoxInput;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (0 != flag--)
+            {
+                buttonCopy.Enabled = false;
+                buttonCopy.Text = "已复制！" + flag.ToString() + "S";
+            }
+            else
+            {
+                flag = 3;
+                timer1.Enabled = false;
+                buttonCopy.Text = "复制";
+                buttonCopy.Enabled = true;
+            }
+        }
+    }
+}
